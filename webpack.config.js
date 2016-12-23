2bb4e3757d2053c4758c680bbd7ee376bc5a82ed
mrbabbs <giovanni.esposito@shore.com>
2016-12-23 14:45:42 +0100

    configure babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,18 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/app.js',
   module: {
-    loader: {
+    loaders: [{
       test: /\.js$/,
       exclude: ['node_modules', 'dist'],
-      loader: 'babel-loader'
-    }
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
   },
   output: {
     path: './dist',
+    publicPath: '/assets/',
     filename: 'app.min.js'
   }
 }
